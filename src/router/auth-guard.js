8da7ce39a618646f00850892e62513b2506e54ca
Patrick Hamelin <13651676+valdrox@users.noreply.github.com>
2018-12-05 17:38:29 +0800

    Fixes redirect on login

--- src/router/auth-guard.js
+++ src/router/auth-guard.js
@@ -4,9 +4,9 @@ export default (to, from, next) => {
   if (to.matched.some(record => record.meta.requiresAuth)) {
     // this route requires auth, check if logged in
     // if not, redirect to login page.
-    if (!store.state.user) {
+    if (!store.state.user.user) {
       next({
-        path: "/signin",
+        path: "/auth",
         query: { redirect: to.fullPath },
       });
     } else {
